5e3c90ff84edf9d7a94e559eb7e510ee9adbe206
!BadRhyme <128097960+BadRhyme@users.noreply.github.com>
2024-06-28 00:35:37 +0300

    Update bombex.py

--- bombex.py
+++ bombex.py
@@ -39,14 +39,15 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!=========================!
+  !=====================!                                                       
+        !BadRhyme               
+     Sms and Caller bot                      
+ Telegram;t.me/badrhymetht
+  !=====================!
+!=========================!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!    
     Sms: {}                         
     Ara: {}              {}by {}@!BadRhyme\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), len(servisler_call), Style.RESET_ALL, Fore.LIGHTRED_EX))
